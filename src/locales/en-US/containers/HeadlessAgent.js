b4aaddd338a0c80cf874690df1bd33c43f7929a7
Shaoke <shaokexu@gmail.com>
2020-06-10 22:07:29 -0700

    change catch to capture

--- src/locales/en-US/containers/HeadlessAgent.js
+++ src/locales/en-US/containers/HeadlessAgent.js
@@ -4,17 +4,20 @@
  * This contains all the text for the LoginPage container.
  */
 export default {
-  'app.containers.HeadlessAgent.subTitle': 'Launch a Chromium instance and execute your Intelligences',
-  'app.containers.HeadlessAgent.description': 'Headless agent is based on <a href="https://pptr.dev/">Puppeteer</a>, and it is good for crawl data from Single Page Application.',
+  'app.containers.HeadlessAgent.subTitle':
+    'Launch a Chromium instance and execute your Intelligences',
+  'app.containers.HeadlessAgent.description':
+    'Headless agent is based on <a href="https://pptr.dev/">Puppeteer</a>, and it is good for crawl data from Single Page Application.',
   'app.containers.HeadlessAgent.saveAndRestart': 'Save & Restart',
   'app.containers.HeadlessAgent.headless': 'Headless Mode',
   'app.containers.HeadlessAgent.headlessDescription':
     'Whether to run browser in <a href="https://developers.google.com/web/updates/2017/04/headless-chrome">headless mode</a>',
-  'app.containers.HeadlessAgent.screenshots': 'Catch Screenshots',
+  'app.containers.HeadlessAgent.screenshots': 'Capture Screenshots',
   'app.containers.HeadlessAgent.screenshotsDescription':
     'Whether to captch screenshots. You can access it by view <span class="ant-typography"><code>http://localhost:Port</code></span>',
   'app.containers.HeadlessAgent.customFunctionTimeout': 'Custom Function Timeout',
-  'app.containers.HeadlessAgent.customFunctionTimeoutInvalid': 'Please enter a valid custom function timeout value',
+  'app.containers.HeadlessAgent.customFunctionTimeoutInvalid':
+    'Please enter a valid custom function timeout value',
   'app.containers.HeadlessAgent.customFunctionTimeoutExample': '60000',
   'app.containers.HeadlessAgent.customFunctionTimeoutDescription':
     'Timeout value for executing custom function, value is based on <span class="ant-typography"><code>ms</code></span>',
